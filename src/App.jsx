c57373c3b5c6cca2221e5e87c2e9adcb42892458
Jermaine <jermaine.aroma@gmail.com>
2024-02-17 20:43:41 +0100

    created separate file for useLocalStorage function. And added extra key for different value

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useLocalStorage } from './useLocalStorage'; 
 
 const App = () => {
-  const [name, setName] = useState(() => {
-    const storedName = window.localStorage.getItem('name');
-    return storedName || 'YourName';
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem('name', name);
-  }, [name]);
+  const [name, setName] = useLocalStorage('name', 'Jacop'); 
+  const [subject, setSubject] = useLocalStorage('subject', 'React Hooks'); 
 
   return (
     <div className="App">
@@ -18,6 +13,14 @@ const App = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Enter your name"
+      />
+
+      <p>Continue learning {subject}.</p>
+      <input
+        type="text"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+        placeholder="Enter subject"
       />
     </div>
   );
